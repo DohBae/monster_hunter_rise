a288f42a2f246d6a180003728f59268b61fbef3c
DohBae <bdoherty92@msn.com>
2022-11-02 11:59:02 -0400

    Create search bar and function to get all monsters in db

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import SearchBar from "./components/SearchBar";
+import axios from 'axios';
 
 function App() {
+  const [allMonsters, setAllMonsters] = useState('');
+
+  // const url = 'http://localhost:8080/';
+
+  useEffect(() => {
+    getAllMonsters();
+  }, []);
+
+  const getAllMonsters = () => {
+    axios.get('http://localhost:8080/monsters')
+    .then((res) => {
+      console.log(Object.values(res.data.monsters))
+    })
+  }
+
+  useEffect(() => {
+    fetch(`/monsters`).then(
+      res => res.json())
+      .then(data => setAllMonsters)
+  })
+
   return (
     <div className="App">
-      Hello there!
+      <SearchBar placeholder="Enter monster name..."/>
     </div>
   );
 }
---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './SearchBar.css';
+import axios from 'axios';
+
+export default function SearchBar({placeholder, data}) {
+  
+  return (
+  <div className="search">
+    <div className="searchInputs">
+      <input type="text" placeholder={placeholder} />
+      <div className="searchIcon"></div>
+    </div>
+    <div className="dataResult"></div>
+  </div>
+  );
+}
