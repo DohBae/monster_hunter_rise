04886fbe3fe0332614ff50e60f1d7a4e323e8981
DohBae <bdoherty92@msn.com>
2022-11-08 13:47:47 -0500

    Add filter for monster names in search bar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -3,21 +3,34 @@ import './SearchBar.css';
 import axios from 'axios';
 
 export default function SearchBar({placeholder, data}) {
-  
+  const [filteredData, setFilteredData] = useState([]);
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value
+    const newFilter = data.filter((value) => {
+      return value.name.includes(searchWord);
+    });
+    setFilteredData(newFilter);
+  }
 
   return (
   <div className="search">
     <div className="searchInputs">
-      <input type="text" placeholder={placeholder} />
+      <input type="text" placeholder={placeholder} onChange={handleFilter} />
       <div className="searchIcon"></div>
     </div>
-    <div className="dataResult">
-      {data.map((value, key) => {
+
+    {filteredData.length != 0 && (
+      <div className="dataResult">
+      {filteredData.map((value, key) => {
         return <a className="dataItem">
           <p>{value.name}</p>
           </a>
       })}
     </div>
+    )
+      
+    }
   </div>
   );
 }
