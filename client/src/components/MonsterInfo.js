b4af5f9dd30c1a0994b132b22ee7349376434b2e
DohBae <bdoherty92@msn.com>
2022-11-10 12:12:33 -0500

    Create MonsterInfo component

---
+++ client/src/components/MonsterInfo.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import '../App.scss';
+import Axios from 'axios';
+
+export default function MonsterInfo() {
+
+  return (
+    <div>Hello There!</div>
+  );
+}
